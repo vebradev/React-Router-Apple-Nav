0e9360ae6eb353ed7a32dab957d96ee3388e9da4
vebradev <justinas@vebra.dev>
2019-06-11 16:55:39 +0300

    Add Nav component with styles.

--- apple-nav/src/components/Nav.js
+++ apple-nav/src/components/Nav.js
@@ -3,19 +3,24 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledNav = styled(NavLink)`
-    color: #fff;
-    text-decoration: none;
-    margin: 0 15px;
+  color: #fff;
+  text-decoration: none;
+  margin: 0 15px;
 
-    &:hover {
-        border-bottom: 2px solid #fff;
-        margin-top: 2px;
-    }
+  &:hover {
+    opacity: 0.6;
+    transition: 0.4s all;
+  }
 `;
 
 class Nav extends React.Component {
   render() {
-    return <StyledNav to={`/${this.props.menu.id}`}>{this.props.menu.title}</StyledNav>;
+    const title = this.props.menu.title.toLowerCase();
+    return (
+      <>
+        <StyledNav to={`/${title}`}>{this.props.menu.title}</StyledNav>
+      </>
+    );
   }
 }
 
