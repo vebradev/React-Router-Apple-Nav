a2012dee8ad774eeb5220924595326ee7dd0af4e
vebradev <justinas@vebra.dev>
2019-06-11 16:55:55 +0300

    Add SubNav component with styles.

---
+++ apple-nav/src/components/SubNav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledSubNav = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #111;
+  font-size: 13px;
+  text-decoration: none;
+  margin: 0 15px;
+  text-align: center;
+  padding: 20px 0;
+
+  &:hover {
+    color: #0070c9;
+  }
+
+  img {
+      display: block;
+      max-width: 48px;
+      max-height: 54px;
+  }
+`;
+
+function SubNav(props) {
+  return (
+    <>
+      {props.subLinks.map(item => (
+        <StyledSubNav key={item.id} to="#">
+          <img src={item.imageUrl} alt={item.title}/>
+          {item.title}
+        </StyledSubNav>
+      ))}
+    </>
+  );
+}
+
+export default SubNav;
